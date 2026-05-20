41829f3d2d60a67fde9883a13f417b95a947ee27
BlackcoinDev <dev@blackcoin.dev>
2026-05-20 18:37:16 +0200

    build: Rename project and update paths to Electrum-BLK

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open('contrib/requirements/requirements-hw.txt') as f:
     requirements_hw = f.read().splitlines()
 
 # load version.py; needlessly complicated alternative to "imp.load_source":
-version_spec = importlib.util.spec_from_file_location('version', 'electrum/version.py')
+version_spec = importlib.util.spec_from_file_location('version', 'electrum_blk/version.py')
 version_module = version = importlib.util.module_from_spec(version_spec)
 version_spec.loader.exec_module(version_module)
 
@@ -35,9 +35,9 @@ data_files = []
 if platform.system() in ['Linux', 'FreeBSD', 'DragonFly']:
     # note: we can't use absolute paths here. see #7787
     data_files += [
-        (os.path.join('share', 'applications'),               ['electrum.desktop']),
-        (os.path.join('share', 'pixmaps'),                    ['electrum/gui/icons/electrum.png']),
-        (os.path.join('share', 'icons/hicolor/128x128/apps'), ['electrum/gui/icons/electrum.png']),
+        (os.path.join('share', 'applications'),               ['electrum-blk.desktop']),
+        (os.path.join('share', 'pixmaps'),                    ['electrum_blk/gui/icons/electrum.png']),
+        (os.path.join('share', 'icons/hicolor/128x128/apps'), ['electrum_blk/gui/icons/electrum.png']),
     ]
 
 extras_require = {
@@ -56,22 +56,22 @@ extras_require['fast'] = extras_require['crypto']
 
 
 setup(
-    name="Electrum",
+    name="Electrum-BLK",
     version=version.ELECTRUM_VERSION,
     python_requires='>={}'.format(MIN_PYTHON_VERSION),
     install_requires=requirements,
     extras_require=extras_require,
-    packages=(['electrum',]
-              + [('electrum.'+pkg) for pkg in
-                 find_packages('electrum', exclude=["tests"])]),
+    packages=(['electrum_blk',]
+              + [('electrum_blk.'+pkg) for pkg in
+                 find_packages('electrum_blk', exclude=["tests"])]),
     package_dir={
-        'electrum': 'electrum'
+        'electrum_blk': 'electrum_blk'
     },
     # Note: MANIFEST.in lists what gets included in the tar.gz, and the
     # package_data kwarg lists what gets put in site-packages when pip installing the tar.gz.
     # By specifying include_package_data=True, MANIFEST.in becomes responsible for both.
     include_package_data=True,
-    scripts=['electrum/electrum'],
+    scripts=['electrum_blk/electrum-blk'],
     data_files=data_files,
     description="Lightweight Bitcoin Wallet",
     author="Thomas Voegtlin",
